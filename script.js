1bb43541792233a787153e1ebf3ddb2f1c036da3
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-09-03 14:15:20 +1200

    append h1 to header

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ const calendar = (rightNow, month, year) => {
   // heading
   let h1 = document.createElement("h1");
     h1.innerText = now.toLocaleString("default", { month: "long" }) + " " + now.getFullYear();
+    header.append(h1);
 
   let table = document.createElement("table");
 
