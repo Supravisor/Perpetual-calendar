da70d0c5616f4386231742f48c11f85b41c12a8b
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-09-11 12:48:09 +1200

    add Easter Monday

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function getEaster(year, weekday) {
   } else if (weekday === "Sunday") {
     return new Date (new Date(year, month - 1, day).setHours(1));
   } else if (weekday === "Monday") {
-    return new Date (new Date(year, month - 1, day + 2).setHours(1));
+    return new Date (new Date(year, month - 1, day + 1).setHours(1));
   }
 
 }
@@ -194,6 +194,11 @@ const calendar = (rightNow, month, year) => {
         // Easter Sunday
         if (new Date(new Date(diff + incrementor).setHours(1)).getMonth() === getEaster(new Date(diff + incrementor).getFullYear(), "Sunday").getMonth() && new Date(new Date(diff + incrementor).setHours(1)).getDate() === getEaster(new Date(diff + incrementor).getFullYear(), "Sunday").getDate()) {
           td.innerHTML += `<ul><li>Easter Sunday</li></ul>`;
+        }
+
+        // Easter Monday
+        if (new Date(new Date(diff + incrementor).setHours(1)).getMonth() === getEaster(new Date(diff + incrementor).getFullYear(), "Monday").getMonth() && new Date(new Date(diff + incrementor).setHours(1)).getDate() === getEaster(new Date(diff + incrementor).getFullYear(), "Monday").getDate()) {
+          td.innerHTML += `<ul><li>Easter Monday</li></ul>`;
         }
 
         if ((month > 3 && new Date(diff + incrementor).getDate() < 7)) {
