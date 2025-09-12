19b0a6d0f6a481d032c70dcd8171103e8b69603b
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-09-12 15:06:35 +1200

    Anzac Day typo

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ const calendar = (rightNow, month, year) => {
 
         // Anzac Day
         if ((new Date(diff + incrementor).getDate() === 26 ||  new Date(diff + incrementor).getDate() === 27) && week[day].innerText === "Monday" && new Date (diff + incrementor).toLocaleString("default", { month: "long" }) === "April") {
-          td.innerHTML += `<ul><li>Labour Day</li></ul>`;
+          td.innerHTML += `<ul><li>Anzac Day</li></ul>`;
         }
 
           // Good Friday
