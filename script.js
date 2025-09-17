6c028ac471abd4d58675a9b9b87dae03a917f2b4
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-09-17 15:25:55 +1200

    Mondayise Canterbury (South) Anniversary

--- script.js
+++ script.js
@@ -247,7 +247,7 @@ const calendar = (rightNow, month, year) => {
         }
 
         // Canterbury (South) Anniversary
-        if (new Date(diff + incrementor).getDate() > 21 && new Date(diff + incrementor).getDate() < 29) {
+        if (new Date(diff + incrementor).getDate() > 21 && new Date(diff + incrementor).getDate() < 29 && week[day].innerText === "Monday") {
           td.innerHTML += `<ul><li>Canterbury (South) Anniversary</li></ul>`;
         }
 
