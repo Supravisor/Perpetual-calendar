21c0a7d6a48c22959ac6e435bfbab80645cc8f8d
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-09-18 16:03:46 +1200

    Matariki 2027

--- script.js
+++ script.js
@@ -219,7 +219,8 @@ const calendar = (rightNow, month, year) => {
           new Date(new Date(new Date(diff + incrementor)).setHours(1)).getTime() === new Date(new Date(new Date("2023, 7, 14")).setHours(1)).getTime() ||
           new Date(new Date(new Date(diff + incrementor)).setHours(1)).getTime() === new Date(new Date(new Date("2024, 6, 28")).setHours(1)).getTime() ||
           new Date(new Date(new Date(diff + incrementor)).setHours(1)).getTime() === new Date(new Date(new Date("2025, 6, 20")).setHours(1)).getTime() ||
-          new Date(new Date(new Date(diff + incrementor)).setHours(1)).getTime() === new Date(new Date(new Date("2026, 7, 10")).setHours(1)).getTime()
+          new Date(new Date(new Date(diff + incrementor)).setHours(1)).getTime() === new Date(new Date(new Date("2026, 7, 10")).setHours(1)).getTime() ||
+          new Date(new Date(new Date(diff + incrementor)).setHours(1)).getTime() === new Date(new Date(new Date("2027, 6, 25")).setHours(1)).getTime()
         ) {
           td.innerHTML += `<ul><li>Matariki</li></ul>`;
         }
